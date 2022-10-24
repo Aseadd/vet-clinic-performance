fda6c799a75567cdc19cd846da24f78cc25760c8
addis <betsega23@gmail.com>
2022-10-25 00:25:06 +0300

    Add schema

--- schema.sql
+++ schema.sql
@@ -45,3 +45,11 @@ CREATE TABLE visits(
   date_of_visit DATE,
   PRIMARY KEY(id)
 );
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+// add schema for optimization 
+
+ CREATE INDEX animal_id_asc  ON visits(animal_id ASC);
+ CREATE INDEX vet_id_asc ON visits (vet_id ASC);
+ CREATE INDEX email_asc ON owners(email ASC);
